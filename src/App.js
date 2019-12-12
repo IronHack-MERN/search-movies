f1e7a415cec9fafb04f4cb13165ffa337f6b2eb8
Elena Piaggio <elenapiaggio@gmail.com>
2019-12-13 00:18:54 +0100

    ADD the fetch and get API results

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import Title from './components/Title';
 import SearchForm from './components/SearchForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Title>
-        Search Movies
-      </Title>
-      <div className='SearchForm-wrapper'>
-        <SearchForm/>
+class App extends Component {
+  state = {
+    results: []
+  }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
+  _renderResults(){
+    const { results } = this.state
+    return results.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Title>
+          Search Movies
+        </Title>
+        <div className='SearchForm-wrapper'>
+          <SearchForm
+            onResults = {this._handleResults}
+          />
+        </div>
+        {
+          this.state.results.length === 0
+          ? <p>Sin resultados</p>
+          : this._renderResults()
+        }
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
